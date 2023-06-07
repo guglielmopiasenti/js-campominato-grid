bf798b968098f8a64a7ee2e204414b24d1f11985
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-07 17:44:34 +0200

    correcting funcion and for loop to render cells

--- script.js
+++ script.js
@@ -4,27 +4,41 @@ console.log("JS OK!");
 
 // # Functions section
 
-// Function that gives back a number, excluding the blacklisted ones.
-const getNonBlRandomNumber = (min, max, blacklist) => {
-    let randomNumber;
-
-    // Keeps getting numbers until random number is present in the blacklist
-    do {
-        randomNumber = Math.floor(Math.random() * (max - min)) + min;
-    } while (blacklist.includes(randomNumber));
-
-    return randomNumber;
+// Function that gives back a number from 0 to 100
+const generateNumbers = () => {
+  for (let i = 1; i <= 100; i++) {
+    console.log(i);
+  }
 }
 
-const number = getNonBlRandomNumber("number");
+// 2c Adding a function to create a cell.
+const createCell = (cellNumber) => {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    cell.innerText = cellNumber;
+
+    return cell;
+}
 
 
 //* Initial operations
 
-// Getting elements from the DOM
+// 2a Getting elements from the DOM
 const grid = document.getElementById("grid");
 
 // preparing initial data
 const rows = 10;
 const cols = 10;
 const totalCells = cols * rows;
+
+// 2b we render the cells
+for (let i = 1; i <= totalCells; i++) {
+
+    const cell = createCell(i);
+    
+
+    cell.addEventListener('click', () =>  {
+        cell.classList.add('clicked');
+    });
+    grid.appendChild(cell);
+}
